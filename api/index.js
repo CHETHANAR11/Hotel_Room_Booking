ddef0d03e9d0e0ff6760fb03b948ad6519e37956
monar-22 <mona06547@gmail.com>
2024-01-12 23:19:39 +0530

    uploadpics

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const User = require('./models/User.js');
 const jwt=require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const imageDownloader=require('image-downloader');
+const multer= require('multer');
+const fs = require('fs');
 
 require('dotenv').config();
 const app=express();
@@ -99,7 +101,21 @@ app.post('/register',async (req,res)=>{
     res.json(newName)
  })
 
- console.log(__dirname);
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos',100),(req,res)=>{
+    const uploadedFiles=[];
+  for(let i=0;i<req.files.length;i++){
+    const {path,originalname}= req.files[i];
+    const parts=originalname.split('.');
+    const ext =parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path,newPath);
+    console.log(newPath);
+    uploadedFiles.push(newPath.replace('uploads/',''));
+    console.log(uploadedFiles);
+  }
+  res.json(uploadedFiles);
+});
  
 app.listen(4000,()=>{
     console.log("Listening");
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -46,6 +46,23 @@ export default function PlacesPage(){
       setPhotoLink('');
     }
 
+    function uploadPhoto(ev){
+      const files = ev.target.files;
+      const data = new FormData();
+      for(let i=0;i< files.length;i++){
+         data.append('photos',files[i]);
+      }
+      
+      axios.post('/upload',data,{
+        headers: {'Content-type':'multipart/form-data'}
+      }).then(response =>{
+        const {data:filenames} = response;
+        setPic(prev =>{
+          return [...prev,...filenames];
+       });
+      })
+    }
+
     return(
         <div>
             {action !== 'new' && (
@@ -77,23 +94,24 @@ export default function PlacesPage(){
                      </input>
                      <button onClick={addPicLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photos</button>
                    </div>
-                   <div className="mt-2 grid grid-cols-3  md:grid-cols-4 lg:grid-cols-6">
-                   
-                   {pic.length > 0 && pic.map(link => (
-                     <div>
-                        {/* {link} */}
-                        <img src={'http://localhost:4000/uploads/'+link} alt=""/>
-                     </div>
-                   ))}
-                   
-                   <button className=" flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-                   </svg>
 
-                    Upload
-                   </button>
+                   <div className="mt-2 grid gap-2 grid-cols-3  md:grid-cols-4 lg:grid-cols-6">
+                      {pic.length > 0 && pic.map(link => (
+                        <div>
+                        {/* {link} */}
+                           <img  className="rounded-2xl" src={'http://localhost:4000/'+link} alt=""/>
+                        </div>
+                      ))}
+
+                       <label className="cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                           <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
+                              <path strokeLinecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                           </svg>
+                           Upload
+                       </label>
                    </div>
+
                    {preInput('Description','Description of place')}
                    
                    <textarea value={description} onChange={ev=>setDescription(ev.target.value)}/>
